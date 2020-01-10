ab828cd9341e3ef7b39aed1c924138ec45776390
Da Cheng <dac@twitter.com>
2020-01-10 00:12:58 -0800

    Job setup

---
+++ src/main/java/org/dac/demo/util/Data.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dac.demo.util;
+
+/**
+ * Provides the default data sets used for the WordCount example program.
+ * The default data sets are used, if no parameters are given to the program.
+ *
+ */
+public class Data {
+	public static final String[] ERROR_LOGS = new String[] {
+		"0:00:00	5.0007.510.011	3",
+		"0:00:00	5.0007.610.011	8",
+		"0:00:00	5.0007.510.011	0",
+		"0:00:01	5.0007.510.011	0",
+		"0:00:01	5.0007.510.011	2",
+		"0:00:01	5.0007.610.011	3",
+		"0:00:02	5.0007.610.011	1",
+		"0:00:02	5.0007.510.011	0",
+		"0:00:03	5.0007.610.011	1",
+		"0:00:03	5.0007.510.011	-2",
+		"0:00:03	5.0007.610.011	-1",
+		"0:00:03	5.0007.510.011	5",
+		"0:00:03	5.0007.610.011	-5",
+		"0:00:03	5.0007.610.011	-1",
+		"0:00:03	5.0007.510.011	5",
+		"0:00:03	5.0007.610.011	-5",
+		"0:00:05	5.0007.510.011	-4",
+		"0:00:05	5.0007.610.011	0",
+		"0:00:05	5.0007.610.011	0",
+		"0:00:05	5.0007.610.011	3",
+		"0:00:06	5.0007.510.011	0",
+		"0:00:06	5.0007.610.011	1",
+		"0:00:06	5.0007.610.011	4",
+		"0:00:07	5.0007.510.011	-3",
+		"0:00:07	5.0007.610.011	1",
+		"0:00:07	5.0007.510.011	2",
+		"0:00:07	5.0007.610.011	-7",
+		"0:00:08	5.0007.510.011	-2",
+		"0:00:08	5.0007.510.011	0",
+		"0:00:08	5.0007.610.011	-2",
+		"0:00:08	5.0007.510.011	-6",
+		"0:00:08	5.0007.610.011	-3",
+		"0:00:08	5.0007.610.011	3",
+		"0:00:09	5.0007.510.011	-1",
+		"0:00:09	5.0007.510.011	1",
+		"0:00:09	5.0007.610.011	-2",
+		"0:00:09	5.0007.510.011	-3",
+		"0:00:09	5.0007.510.011	1",
+		"0:00:09	5.0007.510.011	5",
+		"0:00:10	5.0007.610.011	-2",
+		"0:00:10	5.0007.610.011	-1",
+		"0:00:10	5.0007.610.011	-1",
+		"0:00:10	5.0007.510.011	5",
+		"0:00:10	5.0007.610.011	4",
+		"0:00:10	5.0007.610.011	4"
+	};
+}
